f21a86e5587544eb5869ccb23a59d3f1a83f9567
richfly <32109737+richfly@users.noreply.github.com>
2023-09-02 12:54:13 -0700

    vercel?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -8,5 +9,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [preact()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
