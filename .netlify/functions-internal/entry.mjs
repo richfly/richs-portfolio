d8751423cb24572e20233a38791b7295433e60e1
richfly <32109737+richfly@users.noreply.github.com>
2023-09-02 12:48:16 -0700

    netlify me

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest.7fcd2857.mjs';
+import { manifest } from './manifest.b6ddb962.mjs';
 import 'preact';
 import 'preact-render-to-string';
 import 'cookie';
